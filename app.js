4cf2f958f7e4e372e1b7f5b5dbb02328d3abc732
eve <eveshino13@gmail.com>
2021-07-09 00:25:02 +0900

    sql

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const path = require('path');
 
 const express = require('express');
-const bodyParser = require('body-parser');
+
 
 const errorController = require('./controllers/error');
+const db = require("./util/database");
 
 const app = express();
 
@@ -13,7 +14,9 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
--- util/database.js
+++ util/database.js
@@ -1,1 +1,11 @@
+const mysql = require("mysql2");
 
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  database: "node-complete",
+  password: "sayuri0210",
+});
+
+module.exports = pool.promise();
+;
