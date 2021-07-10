3e47e45b95f5192c2b7c6f6d9d1256de8faa28cd
eve <eveshino13@gmail.com>
2021-07-10 15:59:52 +0900

    added delete product

--- app.js
+++ app.js
@@ -3,28 +3,36 @@ const path = require('path');
 const express = require("express");
 
 const errorController = require('./controllers/error');
-
-const mongoConnect = require("./util/database");
+const mongoConnect = require("./util/database").mongoConnect;
+const User = require("./models/user");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-// const adminRoutes = require('./routes/admin');
-// const shopRoutes = require('./routes/shop');
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-    // app.use('/admin', adminRoutes);
-    // app.use(shopRoutes);
+app.use((req, res, next) => {
+  User.findById("60e89a88e30e66bb08f65d3e")
+    .then((user) => {
+      req.user = new User(user.name, user.email, user.cart, user._id);
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect((client) => {
-    console.log(client)
-    app.listen(3000);
+mongoConnect(() => {
+  app.listen(3000);
 });
 
 
--- util/database.js
+++ util/database.js
@@ -1,15 +1,32 @@
 const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
 
-const mongoConnect = (callback) =>   {
-    MongoClient.connect('mongodb://nodejsraw:everson13@cluster0-shard-00-00.wu6no.mongodb.net:27017,cluster0-shard-00-01.wu6no.mongodb.net:27017,cluster0-shard-00-02.wu6no.mongodb.net:27017/nodejs?ssl=true&replicaSet=atlas-n1unka-shard-0&authSource=admin&retryWrites=true&w=majority')
-        .then(client => {
-            console.log('Connected!')
-            callback(client)
-        })
-        .catch(err => {
-            console.log(err)
-        })
-        }
+let _db;
 
-module.exports = mongoConnect
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    "mongodb://nodejsraw:everson13@cluster0-shard-00-00.wu6no.mongodb.net:27017,cluster0-shard-00-01.wu6no.mongodb.net:27017,cluster0-shard-00-02.wu6no.mongodb.net:27017/nodejs?ssl=true&replicaSet=atlas-n1unka-shard-0&authSource=admin&retryWrites=true&w=majority",
+    {
+      useUnifiedTopology: true,
+    }
+  )
+    .then((client) => {
+      console.log("Connected!");
+      _db = client.db();
+      callback();
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+};
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw "Database not found";
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
